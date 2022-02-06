cce31c8feca3b3cca487c8301c345365cbc928ca
ikegam1 <takesiba.masrao@gmail.com>
2022-02-06 18:39:18 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import { Frame417, FormCheckout } from './ui-components';
+import { FrameExampleBCollection, FormCheckout } from './ui-components';
 
 function App() {
   return (
     <div className="App">
-    <Frame417 width={{ small: "300px", large: "600px", xl: "800px" }}/>
-    <FormCheckout width={{ small: "300px", large: "600px", xl: "800px" }}/>
+    <h1>あああ</h1>
+    <FrameExampleBCollection 
+      justifyContent="flex-start"
+      isPaginated itemsPerPage={3} 
+      width={{ small: "300px", large: "600px", xl: "800px" }}/>
     </div>
   );
 }
